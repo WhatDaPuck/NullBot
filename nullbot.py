261c20b838d30af0393b792a4c10a4c40f870d1a
WhatDaPuck <cbock1337@gmail.com>
2017-07-24 21:35:26 -0400

    added more features

--- nullbot.py
+++ nullbot.py
@@ -9,12 +9,14 @@ from discord.ext import commands
 
 client = discord.Client()
 logging.basicConfig(level=logging.INFO)
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print()
+    await client.change_presence(game=discord.Game(name='0x90'), status=None, afk=False)
 
 
 @client.event
@@ -28,7 +30,11 @@ async def on_message(message):
     if message.content.startswith("!null"):
         role = discord.utils.get(message.server.roles, name="0xNULL")
         await client.add_roles(message.author, role)
+        await client.change_nickname(message.author, "null")
         await client.send_message(message.channel, "welcome to null")
+        await asyncio.sleep(5)
+        async for msg in client.logs_from(message.channel):
+            await client.delete_message(msg)
     elif message.content.startswith("^test"):
         await client.send_message(message.channel, "i'm working")
 
